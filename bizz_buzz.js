da06e91a37979b8d2d3d0e7813065cd24a7140fe
Pavlo Kantsir <34620231+Pashkaua@users.noreply.github.com>
2018-04-19 18:51:05 +0300

    second task

---
+++ bizz_buzz.js
@@ -0,0 +1,18 @@
+function prin(N) {
+  for(let i = 1; i < N; i++){
+   
+    if(i%3==0 && i%5==0){
+      console.log("Bizz Buzz");
+    }
+    else if (i%3 == 0){
+      console.log("Bizz");
+    }
+    else if(i%5==0){
+      console.log("Buzz");
+    }
+    else{
+        //console.log(i);  
+    }
+  }
+}
+prin(100);
